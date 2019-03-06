342bb91c8e4f270014ad668e22540f638e287b52
samalo <smhonds@gmail.com>
2019-03-06 17:21:31 +0200

    'Addition'

--- server/server.js
+++ server/server.js
@@ -21,9 +21,10 @@ io.on('connection', (socket) => {
     
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
- socket.on('createMessage', (message) => {
-  console.log('Create messege', message);
+ socket.on('createMessage', (message, callback) => {
+  console.log('createMessege', message);
   io.emit('newMessage', generateMessage(message.from, message.text));
+  callback('This is from the server!');
 
   //Broadcasting
   // socket.broadcast.emit('newMessage', {
